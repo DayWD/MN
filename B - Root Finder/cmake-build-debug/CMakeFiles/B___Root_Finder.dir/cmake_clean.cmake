214c64144e9719fe91d14ba73585c965de809ad4
DayWD <DayWeed@github.com>
2021-04-15 15:59:27 +0200

    Working Version

---
+++ B - Root Finder/cmake-build-debug/CMakeFiles/B___Root_Finder.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "B___Root_Finder.exe"
+  "B___Root_Finder.exe.manifest"
+  "B___Root_Finder.pdb"
+  "CMakeFiles/B___Root_Finder.dir/main.cpp.obj"
+  "libB___Root_Finder.dll.a"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/B___Root_Finder.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
