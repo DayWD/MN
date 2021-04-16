fecd8584d8c6e4dd3bcc0b9a4e495748b35a8727
DayWD <DayWeed@github.com>
2021-04-16 19:02:16 +0200

    findCurve wstępnie działa

--- D - Metoda Newtona/source.cpp
+++ D - Metoda Newtona/source.cpp
@@ -1,16 +1,13 @@
 //Dawid Urbanik
 using namespace std;
 
-void printVector(const double* x, unsigned N){
-    for(unsigned i=0;i<N;++i)
-        printf("%17.17f ",x[i]);
+void printVector(const double *x, unsigned N) {
+    for (unsigned i = 0; i < N; ++i)
+        printf("%17.17f ", x[i]);
     printf("\n");
 }
-//      x - tablica długości N, określająca wektor, dla którego liczona jest wartość funkcji
-//      y - tablica długości M, do której funkcja wpisze obliczone f(x). Funkcja zakłada, że tablica y ma co najmniej M elementów!
-//      Df - tablica długości M*N, do której funkcja wpisze obliczoną macierz różniczki Df(x). Funkcja zakłada, że tablica Df ma co najmniej N*M elementów!
 
-typedef void (*FuncPointer)(const double* x, double* y, double* Df);
+typedef void (*FuncPointer)(const double *x, double *y, double *Df);
 
 
 //        f - wskaźnik do funkcji R3 -> R2
@@ -18,33 +15,33 @@ typedef void (*FuncPointer)(const double* x, double* y, double* Df);
 //        k - określa liczbę punktów do wyznaczenia
 //        h - krok zmiany parametru. Funkcję f możemy traktować jako   f(a,b,c) = (f1(a,b,c),f2(a,b,c))
 
-double* Matrix_Inversion_2x2(double a,double b,double c,double d)
-{
-    double det = (a*d-b*c);
-    static double M[4] = {d/det,-b/det,-c/det,a/det};
-    return M;
+inline double *Matrix_Inversion_2x2(double a, double b, double c, double d) {
+    double det = (a * d - b * c);
+    return new double[4]{d / det, -b / det, -c / det, a / det};
 }
 
-int findCurve(FuncPointer f, double* x, unsigned k, double h)
-{
-    cout.precision(17);
+int findCurve(FuncPointer f, double *x, unsigned k, double h) {
     double Df[6];
-    double *Dfc;
-    double M_temp[2];
 
-    for(int i=1;i<=k;i++) {
-        double y[2]={1};
-        double next_iter[3]={x[0],x[1],x[2]+i*h};
-        while (abs(y[0]) >= pow(10, -13) || abs(y[1]) >= pow(10, -13)) {
+    for (int i = 1; i <= k; i++) {
+
+        double y[2] = {1};
+        double next_iter[3] = {x[0], x[1], x[2] + i * h};
+
+        while (abs(y[0]) >= pow(10, -14) || abs(y[1]) >= pow(10, -14)) {
+
+            double y_temp[2] = {abs(y[0]), abs(y[1])};
+
             f(next_iter, y, Df);
-            Dfc = Matrix_Inversion_2x2(Df[0], Df[1], Df[3], Df[4]);
-            M_temp[0] = Dfc[0] * y[0] + Dfc[1] * y[1];
-            M_temp[1] = Dfc[2] * y[0] + Dfc[3] * y[1];
-            next_iter[0] = next_iter[0] - M_temp[0];
-            next_iter[1] = next_iter[1] - M_temp[1];
+
+            if (y_temp[0] < abs(y[0]) && y_temp[1] < abs(y[1]))
+                return i;
+
+            auto Dfc = Matrix_Inversion_2x2(Df[0], Df[1], Df[3], Df[4]);
+            next_iter[0] -= Dfc[0] * y[0] + Dfc[1] * y[1];
+            next_iter[1] -= Dfc[2] * y[0] + Dfc[3] * y[1];
         }
-//    printVector(next_iter,3);
-        cout << next_iter[0] << "  " << next_iter[1] << "  " << next_iter[2] << endl;
+        printVector(next_iter, 3);
     }
 
     return 0;
